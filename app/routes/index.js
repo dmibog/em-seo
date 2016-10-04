1ede6e22f4e1a067550f04674d0b3c5918461053
dmitry <dmb@softgrad.com>
2016-10-04 10:26:56 +0300

    added route and simple template

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import fetch from 'ember-network/fetch';
+
+export default Ember.Route.extend({
+  model() {
+    return fetch('https://api.github.com/users/tomdale').then((response) => {
+      return response.json();
+    });
+  }
+});
